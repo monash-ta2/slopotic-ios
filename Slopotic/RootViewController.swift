5980a2fef543e857bef0663988224485a330eafa
Weiyi Kong <git@momok.cyou>
2022-03-30 18:05:06 +1100

    init: new project
    Setup with XcodeGen, Main Storyboard deleted.

---
+++ Slopotic/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  Slopotic
+//
+//  Created by Weiyi Kong on 30/3/2022.
+//  Copyright © 2022 Double Flash. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
